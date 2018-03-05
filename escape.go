5ac5180be9c4eb1e90aa5c65e1f1c7c2f1e76e5a
Daniel Rigberg <daniel.rigberg@gmail.com>
2018-03-05 11:32:33 +0100

    build out room, incomplete

--- escape.go
+++ escape.go
@@ -6,6 +6,32 @@ import (
 	"os"
 )
 
+type Room struct {
+	description string
+	queryText string
+	options []string
+	results []string
+}
+
+func printWithNewline(text string) {
+	fmt.Print(text, "\n")
+}
+
+func (room Room) query() string {
+	printWithNewline(room.description)
+	printWithNewline(room.queryText)
+
+	for i := range room.options {
+		fmt.Printf("%d: %s", i, room.options[i])
+	}
+
+	printWithNewline("")
+
+	action, _ := reader.ReadString('\n')
+
+	return room.results[int(action)]
+}
+
 var (
 	username string
 	reader = bufio.NewReader(os.Stdin)
@@ -16,7 +42,16 @@ func getUserName() {
 	username, _ = reader.ReadString('\n')
 }
 
+func explore(room Room) {
+
+}
+
 func main() {
 	getUserName()
 	fmt.Printf("Hello, %s", username)
+	room := Room{"You are in a room.", "What do you want to do?", []string{"Go to the door"}, []string{"You go to the door."}}
+
+	action := room.query()
+
+	fmt.Print(action)
 }
