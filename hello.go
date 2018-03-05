0199d868f5a38811fd24cc85083f7c53a4317572
Daniel Rigberg <daniel.rigberg@gmail.com>
2018-03-05 10:25:44 +0100

    linting

--- hello.go
+++ hello.go
@@ -10,10 +10,12 @@ var (
 	username string
 	reader = bufio.NewReader(os.Stdin)
 )
+
 func getUserName() {
 	fmt.Print("Enter username: ")
 	username, _ = reader.ReadString('\n')
 }
+
 func main() {
 	getUserName()
 	fmt.Printf("Hello, %s", username)
